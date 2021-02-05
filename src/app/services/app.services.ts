d3f2bba1577adba1461e4acab479400e37d5a25c
Elena Borodina <eborodina@verenia.com>
2021-02-05 08:39:00 +0200

    add rxjs

--- src/app/services/app.services.ts
+++ src/app/services/app.services.ts
@@ -1,8 +1,17 @@
-import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { catchError } from 'rxjs/internal/operators/catchError';
-import { of } from 'rxjs/internal/observable/of';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+interface Client {
+    name: string,
+    id: string | number,
+    services?: Service[]
+}
+interface Service {
+    name: string,
+    id: string | number
+}
 
 @Injectable({
     providedIn: 'root'
@@ -16,34 +25,58 @@ export abstract class AppService {
     ) {
     }
 
-    getClientsList(): Observable<any> {
+    getClientsList(): Observable<Client[]> {
         return this.httpClient
             .get(
                 `${this.url}/Clients/GetClients`
+            ).pipe(
+                map((data: Client[]) => {
+                    return data;
+                }), catchError(error => {
+                    return throwError("Can't get clients list");
+                })
             )
     }
 
-    getClient(id): Observable<any> {
+    getClient(id): Observable<Client> {
         return this.httpClient
             .get(
                 `${this.url}/Clients/UpdateClient/?id=${id}`
+            ).pipe(
+                map((data: Client) => {
+                    return data;
+                }), catchError(error => {
+                    return throwError("Can't get client");
+                })
             )
     }
 
-    getServicesList(): Observable<any> {
+    getServicesList(): Observable<Service[]> {
         return this.httpClient
             .get(
                 `${this.url}/Services/GetServices`
+            ).pipe(
+                map((data: Service[]) => {
+                    return data;
+                }), catchError(error => {
+                    return throwError("Can't get service list");
+                })
             )
     }
 
-    updateClient(data): Observable<any> {
+    updateClient(data): Observable<Client> {
         return this.httpClient
             .post(
                 `${this.url}/Clients/UpdateClient`,
                 data
+            ).pipe(
+                map((data: Client) => {
+                    return data;
+                }), catchError(error => {
+                    return throwError("Can't update client");
+                })
             )
     }
 
-    
+
 }
